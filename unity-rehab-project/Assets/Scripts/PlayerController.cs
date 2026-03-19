813c2443a55764fc51f95b156d2091ee64fcff6a
Daehyeon Kim <daehyeon.dev@gmail.com>
2026-03-19 19:01:38 +0900

    Add Player Move
    플레이어 이동(Rigidbody2D 사용) 추가

--- unity-rehab-project/Assets/Scripts/PlayerController.cs
+++ unity-rehab-project/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,42 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private Rigidbody rbc;
+    private float moveInput;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        rbc = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        moveInput = Input.GetAxisRaw("Horizontal");
+
+        FlipCharacter();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+    }
+
+    private void FlipCharacter()
+    {
+        if (moveInput == 0)
+            return;
+
+        Vector3 scale = transform.localScale;
+        scale.x = Mathf.Abs(scale.x) * Mathf.Sign(moveInput);
+        transform.localScale = scale;
     }
 }

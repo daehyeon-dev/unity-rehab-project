848efc1bfc5056fb346fb1633db85a1a3e475554
Daehyeon Kim <daehyeon.dev@gmail.com>
2026-03-20 19:26:22 +0900

    Add Enemy Controll
    적 이동 간단하게 구현
    -시작점 기준으로 좌우 반복 이동
    -Gizmo 사용해서 범위 확인

--- unity-rehab-project/Assets/Scripts/EnemyController.cs
+++ unity-rehab-project/Assets/Scripts/EnemyController.cs
@@ -1,18 +1,76 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float moveSpeed = 2f;
+    [SerializeField] private float moveRange = 2f;
+
+    private Vector2 startPosition;
+    private int direction = 1;
+
+    private Rigidbody2D rb;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    private void Start()
+    {
+        startPosition = transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        rb.velocity = new Vector2(direction * moveSpeed, rb.velocity.y);
+
+        if(transform.position.x >= startPosition.x + moveRange)
+        {
+            direction = -1;
+            Flip();
+        }
+        else if(transform.position.x < startPosition.x - moveRange)
+        {
+            direction = 1;
+            Flip();
+        }
+    }
+
+    private void Flip()
+    {
+        Vector3 scale = transform.localScale;
+        scale.x = Mathf.Abs(scale.x) * direction;
+        transform.localScale = scale;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+
+        Vector3 center;
+        if (Application.isPlaying)
+        {
+            center = new Vector3(startPosition.x, transform.position.y, transform.position.z);
+        }
+        else
+        {
+            center = transform.position;
+        }
+
+        Vector3 left = center + Vector3.left * moveRange;
+        Vector3 right = center + Vector3.right * moveRange;
+
+        Gizmos.DrawLine(left, right);
+        Gizmos.DrawSphere(left, 0.1f);
+        Gizmos.DrawSphere(right, 0.1f);
     }
 }
